108818dccd7ce355d9603375b09ba042d1edf357
j88moja-code <sitholent+github@gmail.com>
2022-08-25 20:15:20 -0700

    Fix error in code for 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,18 +2,14 @@
 
 if __name__ == "__main__":
     from sys import argv
+    from calculator_1 import add, sub, mul, div
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         quit(1)
     a = int(argv[1])
     b = int(argv[3])
-    ops = ["+", "-", "*", "/"]
-    from calculator_1 import add, sub, mul, div
-    funcs = [add, sub, mul, div]
-    for i, s in enumerate(ops):
-        if argv[2] == s:
-            print("{} {} {} = {}".format(a, s, b, funcs[i](a, b)))
-            break
-        else:
-            print("Unkown operator. Available operators: +, -, * and /")
-            quit(1)
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if argv[2] not in list(ops.keys()):
+        print("Unkown operator. Available operators    : +, -, * and /")
+        quit(1)
+    print("{} {} {} = {}".format(a, argv[2], b, ops[argv[2]](a, b)))
