12e199c173cedb3a196723e8b07a83823a00fdb6
j88moja-code <sitholent+github@gmail.com>
2022-09-26 16:14:22 -0700

    Square #2

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Subclass Rectangle
+Subclass Square
+Class BaseGoemetry
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ New Subclass"""
+    def __init__(self, size):
+        """ Creates a square by using the super() method"""
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """ Strig representation of a Square"""
+        return "[Square] " + str(self.__size) + "/" + str(self.__size)
