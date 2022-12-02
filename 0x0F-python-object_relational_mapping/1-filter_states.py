3588953b0fed19f7ade911c1afc615a20098b59e
j88moja-code <sitholent+github@gmail.com>
2022-12-02 03:06:50 -0800

    1. Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
+"""MySQLdb module for python (MySQL Python) usage
+"""
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
+    """The code is not executed when imported
+    """
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3],
                          charset="utf8")
     c = db.cursor()
-    c.execute("SELECT * FROM states ORDER BY states.id ASC")
+    c.execute("SELECT * FROM `states` ORDER BY states.id ASC")
 
     query_result = c.fetchall()
     if query_result:
