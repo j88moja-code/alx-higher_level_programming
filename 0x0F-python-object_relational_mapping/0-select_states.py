f5bf3a0424412f4a98c14bfcfbeec715171d297f
j88moja-code <sitholent+github@gmail.com>
2022-12-02 03:10:54 -0800

    Fix pycodestyle errors

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+"""MySQLdb module for python (MySQL Python) usage
+"""
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
+    """this code is executable when not imported
+    """
     db = MySQLdb.connect(user=sys.argv[1], password=sys.argv[2],
                          db=sys.argv[3], charset="utf8")
     c = db.cursor()
