7c907a981748f265110f04fd59cf0ced5fd09127
j88moja-code <sitholent+github@gmail.com>
2022-12-02 03:48:43 -0800

    4. Cities by states

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""MySQLdb module for python (MySQL Python) usage
+returns all rows in a table
+"""
+import MySQLdb
+import sys
+if __name__ == "__main__":
+    """The code is not executed when imported
+    """
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], charset="utf8")
+    c = db.cursor()
+    c.execute("SELECT `c`.`id`, `c`.`name`, `s`.`name` \
+                 FROM `cities` as `c` \
+                INNER JOIN `states` as `s` \
+                   ON `c`.`state_id` = `s`.`id` \
+                ORDER BY `c`.`id`")
+    [print(city) for city in c.fetchall()]
