e57e9c02c529591da83f0562cf54f9e9fe9ad6d0
j88moja-code <sitholent+github@gmail.com>
2022-12-03 23:41:12 -0800

    13. Delete states - fix errors

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -5,21 +5,24 @@ with a name containing the letter `a`
 from the database `hbtn_0e_6_usa`.
 """
 
-import sys
+from sys import argv
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
 
 if __name__ == "__main__":
     """
     Deletes State objects on the database.
     """
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+    engine = create_engine(db_uri)
     Session = sessionmaker(bind=engine)
+
     session = Session()
 
-    state = session.query(State).filter_by(id=2).first()
-    state.name = "New Mexico"
+    for instance in session.query(State).filter(State.name.contains('a')):
+        session.delete(instance)
+
     session.commit()
+    session.close()
